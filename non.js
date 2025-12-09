4d8c431c17aeeb2312193b3ef6e177b2d9e3161d
themostfluffy <Fuzzyfox9876@gmail.com>
2025-12-09 21:19:03 +0000

    lol

--- non.js
+++ non.js
@@ -1,37 +1,42 @@
-  var money = 0;
-  var mult = 1;
-  var pestige = 1;
-  function setup() {
-createCanvas(1500, 835);
+let img;
+var money = 0;
+var mult = 1;
+var pestige = 1;
+function preload() {
+  img = loadImage("russia.png");
+}
+function setup() {
+  createCanvas(1500, 835);
 }
 
 function draw() {
-
   background(0, 0, 0);
+  if (img) image(img, 0, 0);
 
   fill(255, 0, 0);
   ellipse(750, 417, 300, 300);
-  
+
   stroke(255, 255, 255);
-  textSize(15);
-  text("Potato Count:", 0, 400);
-  text("farms (cost: 200 potato) press m to buy", 0, 300);
-  text("plants per farm (cost: 190 farms) press p to buy reg:200  farms", 0, 200);
-  text(money, 0, 420);
-  text(mult, 0, 320);
-  text(pestige, 0, 220);
-  
-  if (mouseIsPressed){ 
+  textSize(50);
+  text ("soviet potato farm simulator",500,100);
+   textSize(25);
+  text("Potato Count:", 0, 500);
+  text("farms (cost: 200 potato) press m to buy", 0, 400);
+  text("plants per farm (cost: 190 farms) press p to buy reg:200  farms",0,300);
+  text(money, 0, 530);
+  text(mult, 0, 430);
+  text(pestige, 0, 330);
+
+  if (mouseIsPressed) {
     money = money + mult * pestige;
-  } 
-
-  if (money >= 200 && keyIsPressed && key=="m"){
-mult = mult + 1;
-money = money - 200;
   }
-   if (mult >= 200 && keyIsPressed && key == "p") {
-     pestige = pestige + 1;
-     mult = mult - 190;
-   }
-}
 
+  if (money >= 200 && keyIsPressed && key == "m") {
+    mult = mult + 1;
+    money = money - 200;
+  }
+  if (mult >= 200 && keyIsPressed && key == "p") {
+    pestige = pestige + 1;
+    mult = mult - 190;
+  }
+}
