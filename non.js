f05a1c0d2ad2f5bca1eaa3e148a43020d6be9ba4
themostfluffy <Fuzzyfox9876@gmail.com>
2025-12-11 16:05:01 +0000

    auto/bigbuys uptdate

--- non.js
+++ non.js
@@ -3,6 +3,7 @@ var money = 0;
 var mult = 1;
 var pestige = 1;
 var veggies = 1;
+var score = 0;
 function preload() {
   img = loadImage("russia.png");
 }
@@ -26,12 +27,18 @@ function draw() {
   text("farms (cost: 200 potato) press m to buy", 0, 400);
   text("plants per farm (cost: 190 farms) press p to buy reg:200  farms",0,300);
   text("potatoes per plant(cost: 190 plants) press c to buy reg:200 plants",0,200);
+  text("score:",1200,100);
+  text("big farm buy (cost: 20,000 potato) press n to buy",0,370);
+  text("big plants per farm buy (cost: 19,990 farms) press o to buy",0,270);
+  text("big potatoes per plant buy (cost: 19,990 plants) press x to buy",0,170);
 
+  text(score,1200,150);
   text(money, 0, 530);
   text(mult, 0, 430);
   text(pestige, 0, 330);
   text (veggies,0,230);
 
+  score= money*(mult*(pestige*veggies))/1000;
   if (mouseIsPressed) {
     money = money + (mult*(pestige*veggies));
   }
@@ -64,4 +71,40 @@ function draw() {
       veggies = veggies + 100;
       pestige = pestige - 19990;
     }
+//huge buys
+        if (money >= 2000000 && keyIsPressed && key == "b") {
+          mult = mult + 10000;
+          money = money - 2000000;
+        }
+        if (mult >= 2000000 && keyIsPressed && key == "i") {
+          pestige = pestige + 10000;
+          mult = mult - 1999990;
+        }
+        if (pestige >= 2000000 && keyIsPressed && key == "z") {
+          veggies = veggies + 10000;
+          pestige = pestige - 1999990;
+        }
+        //auto
+if (money>= 5000000000) {
+  money = money + (mult*(pestige*veggies));
 }
+if (mult>= 5000000000 && money>=5000000000) {
+  mult = mult +5000000000;
+  money = money - 5000000000;
+}
+
+if (pestige >= 5000000000 && mult >= 4999999990) {
+  pestige = pestige + 5000000000;
+  mult = mult - 4999999990;
+}
+
+if (veggies>= 5000000000 && pestige >= 4999999990) {
+  veggies = veggies +5000000000;
+  pestige = pestige - 4999999990;
+}
+
+
+
+
+}
+
